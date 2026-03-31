609dede978592f71635134b7700de04e3cda8dc9
fearlessav <nnyamida@gmail.com>
2026-03-31 10:45:15 +0100

    Create JWT token verification middleware for protected routes

---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware for JWT token verification
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    
+    if (!token) return res.sendStatus(401); // Unauthorized
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); // Forbidden
+        req.user = user;
+        next(); // Proceed to the next middleware or route handler
+    });
+}
+
+module.exports = authenticateToken;
